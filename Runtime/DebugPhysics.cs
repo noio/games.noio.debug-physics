6b07868eaa9a2714e54264ec60af24309f22bc86
Thomas van den Berg <thomas.g.vandenberg@gmail.com>
2024-07-15 09:08:37 +0200

    correctly pass QueryTriggerInteraction

--- Runtime/DebugPhysics.cs
+++ Runtime/DebugPhysics.cs
@@ -4,41 +4,44 @@ public static class DebugPhysics
 {
     public static readonly float DefaultDrawDuration = .2f;
     public static float DrawDuration { get; set; } = 0f;
-    public static Color ColorHint { get; set; } = new Color(0.74f, 0.8f, 0.94f);
+    public static Color ColorHit { get; set; } = new Color(0.94f, 0.23f, 0.24f);
+    public static Color ColorNoHit { get; set; } = new Color(0.44f, 0.94f, 0.32f);
 
     public static bool Raycast(
-        Vector3                 origin,
-        Vector3                 direction,
-        out RaycastHit          hitInfo,
-        float                   maxDistance             = Mathf.Infinity,
-        int                     layerMask               = Physics.DefaultRaycastLayers,
+        Vector3 origin,
+        Vector3 direction,
+        out RaycastHit hitInfo,
+        float maxDistance = Mathf.Infinity,
+        int layerMask = Physics.DefaultRaycastLayers,
         QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
-        var didHit = Physics.Raycast(origin, direction, out hitInfo, maxDistance, layerMask);
+        var didHit = Physics.Raycast(origin, direction, out hitInfo, maxDistance, layerMask,
+            queryTriggerInteraction);
 #if DEBUG
         direction.Normalize();
         if (didHit)
         {
             var hitPoint = hitInfo.point;
-            Debug.DrawLine(origin, hitPoint, ColorHint, DrawDuration);
-            DebugDraw.WireDisc(hitPoint, hitInfo.normal, .2f * maxDistance, ColorHint, DrawDuration);
-            var color2 = ColorHint;
-            color2.a = .5f;
-            Debug.DrawLine(hitPoint, origin + direction * maxDistance, color2, DrawDuration);
+            Debug.DrawLine(origin, hitPoint, ColorHit, DrawDuration);
+            var restOfLineColor = ColorHit;
+            restOfLineColor.a = .5f;
+            Debug.DrawLine(hitPoint, origin + direction * maxDistance, restOfLineColor, DrawDuration);
+
+            DebugDraw.WireDisc(hitPoint, hitInfo.normal, .2f * maxDistance, ColorHit, DrawDuration);
         }
         else
         {
-            Debug.DrawLine(origin, origin + direction * maxDistance, ColorHint, DrawDuration);
+            Debug.DrawLine(origin, origin + direction * maxDistance, ColorNoHit, DrawDuration);
         }
 #endif
         return didHit;
     }
 
     public static bool Raycast(
-        Ray                     ray,
-        out RaycastHit          hitInfo,
-        float                   maxDistance             = Mathf.Infinity,
-        int                     layerMask               = Physics.DefaultRaycastLayers,
+        Ray ray,
+        out RaycastHit hitInfo,
+        float maxDistance = Mathf.Infinity,
+        int layerMask = Physics.DefaultRaycastLayers,
         QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
         return Raycast(ray.origin, ray.direction, out hitInfo, maxDistance, layerMask,
@@ -48,104 +51,90 @@ public static class DebugPhysics
     public static bool CheckCapsule(
         Vector3 start,
         Vector3 end,
-        float   radius,
-        int     layerMask = Physics.DefaultRaycastLayers,
-        Color?  color     = null
+        float radius,
+        int layerMask = Physics.DefaultRaycastLayers,
+        QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
     )
     {
-#if !DEBUG
-        return Physics.CheckCapsule(start, end, radius, layerMask);
-#else
-        if (color == null)
-        {
-            color = Color.green;
-        }
-
-        var overlap = Physics.CheckCapsule(start, end, radius, layerMask);
-        DebugDraw.WireCapsule(start, end, radius, overlap ? Color.red : color.Value, DrawDuration);
-        return overlap;
+        var didHit = Physics.CheckCapsule(start, end, radius, layerMask, queryTriggerInteraction);
+#if DEBUG
+        DebugDraw.WireCapsule(start, end, radius, didHit ? ColorHit : ColorNoHit, DrawDuration);
 #endif
+        return didHit;
     }
 
     public static bool CheckSphere(
         Vector3 position,
-        float   radius,
-        int     layerMask = Physics.DefaultRaycastLayers,
-        Color?  color     = null
+        float radius,
+        int layerMask = Physics.DefaultRaycastLayers,
+        QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
     )
     {
-#if !DEBUG
-        return Physics.CheckSphere(position, radius, layerMask);
-#else
-        if (color == null)
-        {
-            color = Color.green;
-        }
-
-        var overlap = Physics.CheckSphere(position, radius, layerMask);
-        DebugDraw.WireSphere(position, radius, overlap ? Color.red : color.Value, DrawDuration);
-        return overlap;
+        var didHit = Physics.CheckSphere(position, radius, layerMask, queryTriggerInteraction);
+#if DEBUG
+        DebugDraw.WireSphere(position, radius, didHit ? ColorHit : ColorNoHit, DrawDuration);
 #endif
+        return didHit;
     }
 
     public static bool CheckBox(
-        Vector3    center,
-        Vector3    extents,
+        Vector3 center,
+        Vector3 extents,
         Quaternion rotation,
-        LayerMask  layerMask,
-        Color?     color = null)
+        LayerMask layerMask,
+        QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
-#if !DEBUG
-        return Physics.CheckBox(center, extents, rotation, layerMask);
-#else
-        if (color == null)
-        {
-            color = Color.green;
-        }
-
-        var overlap = Physics.CheckBox(center, extents, rotation, layerMask);
-        DebugDraw.WireCube(center, extents, rotation, overlap ? Color.red : color.Value, DrawDuration);
-        return overlap;
+        var didHit = Physics.CheckBox(center, extents, rotation, layerMask);
+#if DEBUG
+        DebugDraw.WireCube(center, extents, rotation, didHit ? ColorHit : ColorNoHit, DrawDuration);
 #endif
+        return didHit;
     }
 
-    public static int OverlapSphereNonAlloc(Vector3 position, float radius, Collider[] results,
-        LayerMask                                   layerMask)
+    public static int OverlapSphereNonAlloc(
+        Vector3 position,
+        float radius,
+        Collider[] results,
+        LayerMask layerMask,
+        QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
-#if !DEBUG
-        return Physics.OverlapSphereNonAlloc(position, radius, results, layerMask);
-#endif
-        var numFound = Physics.OverlapSphereNonAlloc(position, radius, results, layerMask);
+        var numHits =
+            Physics.OverlapSphereNonAlloc(position, radius, results, layerMask, queryTriggerInteraction);
+#if DEBUG
         DebugDraw.WireSphere(position, radius, Color.white, DrawDuration);
-        for (int i = 0; i < numFound; i++)
+        for (int i = 0; i < numHits; i++)
         {
             var collider = results[i];
-            DebugDraw.WireCube(collider.bounds.center, collider.bounds.size, Quaternion.identity, Color.green,
+            DebugDraw.WireCube(collider.bounds.center, collider.bounds.size, Quaternion.identity, ColorHit,
                 DrawDuration);
         }
+#endif
 
-        return numFound;
+        return numHits;
     }
 
     public static bool SphereCast(
-        Vector3                 origin,
-        float                   radius,
-        Vector3                 direction,
-        out RaycastHit          hitInfo,
-        float                   maxDistance             = Mathf.Infinity,
-        int                     layerMask               = Physics.DefaultRaycastLayers,
+        Vector3 origin,
+        float radius,
+        Vector3 direction,
+        out RaycastHit hitInfo,
+        float maxDistance = Mathf.Infinity,
+        int layerMask = Physics.DefaultRaycastLayers,
         QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
-        var didHit = Physics.SphereCast(origin, radius, direction, out hitInfo, maxDistance, layerMask);
+        var didHit = Physics.SphereCast(origin, radius, direction, out hitInfo, maxDistance, layerMask,
+            queryTriggerInteraction);
 #if DEBUG
         direction.Normalize();
         if (didHit)
         {
             var hitPoint = hitInfo.point;
             Debug.DrawLine(origin, hitPoint, Color.white, DrawDuration);
-            DebugDraw.WireDisc(hitPoint, hitInfo.normal, .1f, Color.red, DrawDuration);
-            var color2 = new Color(1, 1, 1, .5f);
-            Debug.DrawLine(hitPoint, origin + direction.normalized * maxDistance, color2, DrawDuration);
+            var restOfLineColor = new Color(1, 1, 1, .5f);
+            Debug.DrawLine(hitPoint, origin + direction.normalized * maxDistance, restOfLineColor,
+                DrawDuration);
+
+            DebugDraw.WireDisc(hitPoint, hitInfo.normal, .1f, ColorHit, DrawDuration);
             DebugDraw.WireCapsule(origin, origin + direction * (hitInfo.distance), radius, Color.red,
                 DrawDuration);
         }
@@ -159,11 +148,11 @@ public static class DebugPhysics
     }
 
     public static bool SphereCast(
-        Ray                     ray,
-        float                   radius,
-        out RaycastHit          hitInfo,
-        float                   maxDistance             = Mathf.Infinity,
-        int                     layerMask               = Physics.DefaultRaycastLayers,
+        Ray ray,
+        float radius,
+        out RaycastHit hitInfo,
+        float maxDistance = Mathf.Infinity,
+        int layerMask = Physics.DefaultRaycastLayers,
         QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
     {
         return SphereCast(ray.origin, radius, ray.direction, out hitInfo, maxDistance, layerMask,
